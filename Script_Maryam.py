4aae09ae2099dfc2b0802460a1fe0c8587cd4405
Abubakar Mahmood Garba <94145681+SadeeqMg01@users.noreply.github.com>
2021-12-02 14:18:01 +0100

    Add files via upload

---
+++ Script_Maryam.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#<-----------Maryam Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Maryam Muhammad Sabiu, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Maryam Muhammad Sabiu")
+print("Email: " + "\t\t\t"+ "maryamsabiu3943@gmail.com")
+print("Slack_username: " + "\t" + "@maryam_muhammad_sabiu")
+print("Area of interest: " + "\t" + "Genomics")
